4b48782772d01d458cf0f5556779fdc965fa66e9
Joshua Mabina <mabinajoshua@gmail.com>
2016-02-22 03:35:05 +0300

    canGetGpsData can get resultJson from the server

--- src/sample/Test.java
+++ src/sample/Test.java
@@ -2,12 +2,37 @@ package sample;
 
 import sample.util.Console;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 public class Test {
 
 	public Test() {
 	}
 
+	public static String canGetGpsData() {
+		String resultJsonString;
+		Ghiro.GhiroBundle ghiroBundle;
+
+		ArrayList<Ghiro.GhiroBundle> ghiroBundleList = new ArrayList<>();
+		ghiroBundleList.add(new Ghiro.GhiroBundle("name", "GPS Test - " + Long.toString(System.currentTimeMillis())));
+		ghiroBundleList.add(new Ghiro.GhiroBundle("description", "This is a description"));
+		ghiroBundleList.add(new Ghiro.GhiroBundle("image", "/home/joshy/tmp/img/gpstestimg.jpg"));
+
+		try {
+			resultJsonString = Ghiro.analyse(Ghiro.Route.NEW_ANALYSIS, ghiroBundleList);
+			Console.out(resultJsonString);
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+
 	public static void main(String[] args) {
 		Console.out("Falcon Test");
+		Test.canGetGpsData();
 	}
 }
