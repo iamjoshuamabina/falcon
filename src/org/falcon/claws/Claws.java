7b82781e2ccd50ec4ab6c8ef192730e43ad38fde
Joshua Mabina <mabinajoshua@gmail.com>
2016-05-26 22:26:27 +0300

    Fix Copymove precision

--- src/org/falcon/claws/Claws.java
+++ src/org/falcon/claws/Claws.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import static org.falcon.util.LogUtils.LOGE;
+import static org.falcon.util.LogUtils.LOGI;
 import static org.falcon.util.LogUtils.makeLogTag;
 
 public class Claws {
@@ -14,8 +15,7 @@ public class Claws {
 	private static final String TAG = makeLogTag(Claws.class);
 
 	public static String analyse(String imagePathString) {
-		String cmdString = "claws -f " + imagePathString + " -o " + Config.ANALYSIS_DIR + " --ela --copymove " +
-				"--quality --json";
+		String cmdString = "claws -f " + imagePathString + " -o " + Config.ANALYSIS_DIR + " --ela --copymove 4 5 --json";
 
 		Process process;
 		//noinspection MismatchedQueryAndUpdateOfStringBuilder
@@ -31,6 +31,7 @@ public class Claws {
 			while ((line = reader.readLine())!= null) {
 				output.append(line).append("\n");
 			}
+			LOGI(TAG, String.valueOf(output));
 		} catch (IOException | InterruptedException e) {
 			LOGE(TAG, e.getMessage());
 		}
