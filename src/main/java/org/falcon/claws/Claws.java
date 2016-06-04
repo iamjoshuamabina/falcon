5ae72a2b8f2ed080e96949d5e349bf7930399821
Joshua Mabina <mabinajoshua@gmail.com>
2016-06-04 12:01:40 +0300

    Initial commit - Big fish

--- src/main/java/org/falcon/claws/Claws.java
+++ src/main/java/org/falcon/claws/Claws.java
@@ -1,13 +1,5 @@
 package org.falcon.claws;
 
-import org.falcon.Config;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import static org.falcon.util.LogUtils.LOGE;
-import static org.falcon.util.LogUtils.LOGI;
 import static org.falcon.util.LogUtils.makeLogTag;
 
 public class Claws {
@@ -15,30 +7,6 @@ public class Claws {
 	private static final String TAG = makeLogTag(Claws.class);
 
 	public static String analyse(String imagePathString) {
-		String cmdString = "claws -f " + imagePathString
-				+ " -o " + Config.ANALYSIS_DIR
-				+ " --ela "
-				+ "--copymove 4 5 "
-				+ "--json";
-
-		Process process;
-		//noinspection MismatchedQueryAndUpdateOfStringBuilder
-		StringBuilder output = new StringBuilder();
-		try {
-			//noinspection CallToRuntimeExecWithNonConstantString
-			process = Runtime.getRuntime().exec(cmdString);
-			process.waitFor();
-			BufferedReader reader =
-					new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-			String line;
-			while ((line = reader.readLine())!= null) {
-				output.append(line).append("\n");
-			}
-			LOGI(TAG, String.valueOf(output));
-		} catch (IOException | InterruptedException e) {
-			LOGE(TAG, e.getMessage());
-		}
-		return String.valueOf(output);
+		return null;
 	}
 }
---
+++ src/test/java/org/falcon/claws/ClawsTest.java
@@ -0,0 +1,15 @@
+package org.falcon.claws;
+
+import junit.framework.TestCase;
+
+public class ClawsTest extends TestCase {
+
+	public void setUp() throws Exception {
+		super.setUp();
+
+	}
+
+	public void tearDown() throws Exception {
+
+	}
+}
