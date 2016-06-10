418452c4c6cdfc63feac71ff4c69c4242c5e124e
Joshua Mabina <mabinajoshua@gmail.com>
2016-06-10 12:44:33 +0300

    Finished commandBuilder

--- src/main/java/org/falcon/claws/Claws.java
+++ src/main/java/org/falcon/claws/Claws.java
@@ -1,12 +1,17 @@
 package org.falcon.claws;
 
-import static org.falcon.util.LogUtils.makeLogTag;
+import org.falcon.Config;
 
 public class Claws {
 
-	private static final String TAG = makeLogTag(Claws.class);
+	public String commandBuilder(String imagePath) {
+		return "claws -f " + imagePath +" -o " + Config.ANALYSIS_DIR
+				+ " --json --ela --copymove";
+	}
 
 	public static String analyse(String imagePathString) {
 		return null;
 	}
+
+
 }
--- src/test/java/org/falcon/claws/ClawsTest.java
+++ src/test/java/org/falcon/claws/ClawsTest.java
@@ -1,15 +1,28 @@
 package org.falcon.claws;
 
 import junit.framework.TestCase;
+import org.falcon.Config;
 
-public class ClawsTest extends TestCase {
+public class ClawsTest extends TestCase
+{
 
-	public void setUp() throws Exception {
+	public void setUp() throws Exception
+	{
 		super.setUp();
 
 	}
 
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception {}
 
+	public void testCommandBuilder()
+	{
+		Claws claws = new Claws();
+		String imagePath = "/home/joshy/tmp/usitupe_taka_hapa_doc.jpg";
+		String expected = "claws -f " + imagePath + " -o "
+				+ Config.ANALYSIS_DIR + " --json --ela --copymove";
+
+		String actual = claws.commandBuilder(imagePath);
+
+		assertEquals("CommandString: ", expected, actual);
 	}
 }
